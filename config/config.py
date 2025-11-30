9a8961f720bad7e4e9c4af20200dbf9a0dd8cef1
julian_17 <julian.stamm@gmx.net>
2025-11-30 19:02:34 +0100

    major cleanup

--- config/config.py
+++ config/config.py
@@ -2,19 +2,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-DATASETS_DIR = BASE_DIR / "datasets"
 MODELS_DIR = BASE_DIR / "models"
 RESULTS_DIR = BASE_DIR / "results"
 IMAGES_DIR = BASE_DIR / "images"
 VIDEOS_DIR = BASE_DIR / "videos"
 
-VEHICLE_PEDESTRIAN_MODEL = MODELS_DIR / "vehicle_pedestrian_detection.pt"
-SIGN_DETECTION_MODEL = MODELS_DIR / "sign_detection.pt"
-SIGN_CLASSIFICATION_MODEL = MODELS_DIR / "traffic_sign_classification.h5"
-LIGHT_DETECTION_CLASSIFICATION_MODEL = MODELS_DIR / "traffic_light_detect_class.pt"
-UNET_LANE_DETECTION_MODEL = MODELS_DIR / "lane_detection_unet.h5"
-SCNN_LANE_DETECTION_MODEL = MODELS_DIR / "scnn.pth"
-CAMERA_CALIBRATION = MODELS_DIR / "camera_calibration.pkl"
-
+VEHICLE_PEDESTRIAN_MODEL = MODELS_DIR / "object" / "vehicle_pedestrian_detection.pt"
+SIGN_DETECTION_MODEL = MODELS_DIR / "traffic_sign" / "ts_det.pt"
+SIGN_CLASSIFICATION_MODEL = MODELS_DIR / "traffic_sign" / "ts_class.pt"
+LIGHT_DETECTION_CLASSIFICATION_MODEL = MODELS_DIR / "traffic_light" / "traffic_lights_yolov8x.pt"
+SCNN_LANE_DETECTION_MODEL = MODELS_DIR / "scnn_lanedet" / "scnn.pth"
 
 BEAMNG_HOME = r'C:\Users\user\Documents\beamng-tech\BeamNG.tech.v0.36.4.0'
